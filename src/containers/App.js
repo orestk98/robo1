24092d418b70c0000cb8a1ced03c622c75681a52
orestk98 <60813740+orestk98@users.noreply.github.com>
2020-11-29 21:09:37 +0100

    second commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox.js';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
 
 
@@ -40,7 +41,9 @@ constructor(){
          <h1>RoboFriends</h1>
          <SearchBox searchChange={this.onSearchChange}/>
          <Scroll>
+          <ErrorBoundry>
           <CardList robots={filteredRobots}/>
+          </ErrorBoundry>
          </Scroll>
         </div>
       )
